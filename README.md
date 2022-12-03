24b92d457fb1e98d639bf8ce6f244fe5d3e8732c
samdunk47 <98485063+samdunk47@users.noreply.github.com>
2022-12-03 23:11:09 +0000

    e

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Python Wordle
 
-This is replica of wordle made in Python
+This is replica of wordle made in Python with pygame, graphically
 
 By Sam Chandler
 
--- main.py
+++ main.py
@@ -1,27 +1,58 @@
 import wordle
+import pygame
+import sys
 
 # wordle.filter_words_func() -> only use if 5 letter word list does not already exist and contain correct words
 
 class Game():
     def __init__(self) -> None:
+        """ Initalises Game class, initialises pygame, creates Game variables """
+        pygame.init()
+        
         self.words = []
+        self.history = []
+        
+        self.window_width = 800
+        self.window_height = 800
+        self.caption = "Wordle by Sam Chandler"
+        self._display_surface = pygame.display.set_mode((self.window_width, self.window_height))
+        pygame.display.set_caption(self.caption)
+        
+        self.colours = {
+            "background": 
+        }
+        self.running = True
         
         self.add_words()
         
     def add_words(self) -> None:
+        """ Add words to an array of words, stores in instance of class """
         file = open("wordle/words/five_letter_words.txt", "r")
         self.words = file.readlines()
     
-    def take_input(self) -> str:
-        user_input = input("")
-        return user_input
-    
-    def logic(self) -> None:
+    def execute(self) -> None:
+        """ Controls while loop of game """
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.quit()
+                    
+            self.render()
+            
+    def render(self) -> None:
+        """ Renders elements onto screen """
+        pygame.display.update()
         
+        
+    def on_event(self, event) -> None:
+        """ Handles events """
+        
+    def quit(self) -> None:
+        """ Exits pygame, then the program """
+        pygame.quit()
+        sys.exit(0)
     
-    
-    
-
+        
 
 if __name__ == "__main__":
     game = Game()
