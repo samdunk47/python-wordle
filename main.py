3172f4fb8c31e3a1c97d0f873ece8d00a9d8705f
samdunk47 <98485063+samdunk47@users.noreply.github.com>
2022-12-04 18:09:31 +0000

    too ez

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ import sys
 # wordle.filter_words_func() -> only use if 5 letter word list does not already exist and contain correct words
 
 class Game():
+    """ Class to control the game """
     def __init__(self) -> None:
         """ Initalises Game class, initialises pygame, creates Game variables """
         pygame.init()
-        
+
         self.words = []
         self.history = []
         
@@ -17,13 +18,40 @@ class Game():
         self.caption = "Wordle by Sam Chandler"
         self._display_surface = pygame.display.set_mode((self.window_width, self.window_height))
         pygame.display.set_caption(self.caption)
+        self.fps_clock = pygame.time.Clock()
+        self.FPS = 60
+        self.fonts = {
+            "bold": pygame.font.SysFont("wordle/assets/ClearSans-Bold.ttf", 100),
+            "thin": pygame.font.SysFont("wordle/assets/ClearSans-Thin.ttf", 100),
+            "light": pygame.font.SysFont("wordle/assets/ClearSans-Light.ttf", 100),
+            "medium": pygame.font.SysFont("wordle/assets/ClearSans-Medium.ttf", 100),
+            "regular": pygame.font.SysFont("wordle/assets/ClearSans-Bold.ttf", 100),
+        } # all fonts
         
         self.colours = {
-            "background": 
-        }
+            "text": "#ffffff",
+            "content_background": "#121213",
+
+            "letter_correct": "#538d4e",
+            "letter_present": "#b59f3b",
+            "letter_absent": "#3a3a3c",
+            "letter_border": "#3a3a3c",
+
+            "keyboard_background": "#818384",
+            "keyboard_correct": "#538d4e",
+            "keyboard_present": "#b59f3b",
+            "keyboard_absent": "#3a3a3c",
+        } # all colours
+        
+        self.test_text = self.fonts["bold"].render("This is a test", True, self.colours["text"])
+        self.text_text_rect = self.test_text.get_rect()
+        self.text_text_rect.center = (self.window_width / 2, self.window_height / 2)
+        
         self.running = True
         
         self.add_words()
+        
+        self.execute()
         
     def add_words(self) -> None:
         """ Add words to an array of words, stores in instance of class """
@@ -38,9 +66,15 @@ class Game():
                     self.quit()
                     
             self.render()
+            self.fps_clock.tick(self.FPS)
             
     def render(self) -> None:
         """ Renders elements onto screen """
+        
+        self._display_surface.fill(self.colours["content_background"])
+        
+        self._display_surface.blit(self.test_text, self.text_text_rect)
+        
         pygame.display.update()
         
         
